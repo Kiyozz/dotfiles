7cc76c2330c428b7ba86a8a22ba3cb0985cf239a
Kévin TURMEL <kevinturmel@gmail.com>
2021-03-08 09:15:20 +0100

    Polybar multiple monitor via xrandr

---
+++ polybar/.polybar.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload default &
+  done
+else
+  polybar --reload default &
+fi
+
